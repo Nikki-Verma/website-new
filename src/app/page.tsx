86f51c8080a02ffdac2ea7aed294391a777dc5b6
Nikki Verma <nikkiverma@Nikkis-Air.lan>
2024-11-06 19:37:46 +0530

    ssr fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,26 @@ import Header from "@/components/Header/Header";
 import AIStack from "@/components/Home/AIStack/AIStack";
 import AcceleratedExecution from "@/components/Home/AcceleratedExecution/AcceleratedExecution";
 import CaseStudy from "@/components/Home/CaseStudy/CaseStudy";
-import CloudDeployment from "@/components/Home/CloudDeployment";
-import Features from "@/components/Home/Features/Features";
 import Hero from "@/components/Home/Hero";
 import Industries from "@/components/Home/Industries/Industries";
-import PlatformBuild from "@/components/Home/PlatformBuild/PlatformBuild";
 import Testimonial from "@/components/Home/Testimonial/Testimonial";
+import dynamic from "next/dynamic";
+
+const CloudDeployment = dynamic(
+  () => import("@/components/Home/CloudDeployment"),
+  {
+    ssr: false,
+  },
+);
+const Features = dynamic(() => import("@/components/Home/Features/Features"), {
+  ssr: false,
+});
+const PlatformBuild = dynamic(
+  () => import("@/components/Home/PlatformBuild/PlatformBuild"),
+  {
+    ssr: false,
+  },
+);
 
 const home = () => {
   return (
