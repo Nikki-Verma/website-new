8f1f7cdb7f75f8be8d9641c3a8a0527b90f66d12
Nikki Verma <nikkiverma@ip-192-168-31-142.ap-south-1.compute.internal>
2024-11-07 12:21:06 +0530

    checking every page meta dataa

---
+++ src/app/simplai-studio/layout.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "This is new meta data for specific page.",
+  description: "This is the description.",
+  openGraph: {
+    title: "SimplAI: Unified AI Platform for Building Gen AI applications",
+    description:
+      " Build LLM-powered apps with SimplAI. Choose from 100+ foundational models, integrate them with RAG and APIs to create agentic workflows and tools with LLM observability.",
+    url: "https://simplai.ai",
+    images: [
+      {
+        url: "https://media-simplai.s3.ap-south-1.amazonaws.com/2024-06-04T23:42:54.584387513-Build_LLM-Powered_Apps_with_Ease2.jpg", // Must be an absolute URL
+      },
+    ],
+  },
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return <>{children}</>;
+};
